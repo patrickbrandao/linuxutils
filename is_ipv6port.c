c6a06f00c9f88f15ffdec4faa94f2e5131bfe771
Patrick Brandão <patrickbrandao@gmail.com>
2016-01-09 20:40:10 -0200

    Update is_ipv6port.c

--- is_ipv6port.c
+++ is_ipv6port.c
@@ -1,236 +1,88 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <netinet/in.h>
 
-
-// testar se string esta no formato ipv6
-// se is_prefix for 1, sera verificado se a mascara foi informada em numero de bits
-// se have_port for 1, sera verificado se a porta foi informada, mas nao e' possivel ter o prefixo e a
-// porta na mesma declaracao
-int is_ipv6(char *str, int is_prefix, int have_port){
-	char atch;
-	char lastch;
-	char nextch;
-	char n2xtch;
-
-	char address[32];
-	int addrlen = 0;
-	char *rest=0;
-
-	register int i, j;
-
-	int prefix = 0;
+// testar se string esta no formato de rede ipv6 com porta
+int is_ipv6_port(char *str){
+	int i, len;
+	struct in6_addr ipv6addr;
+	char addr[INET6_ADDRSTRLEN + 9];
+	char comp[INET6_ADDRSTRLEN + 9];
+	int idx1 = 0, idx2 = 0;
 	int port = 0;
+	int commacount = 0;
 
-	int len;
+	// controle de "[ ipv6 ]:xx" em relacao ao '[' e ']'
+	int _status = 0; // 0=nao aberto, 1=aberto, 2=fechado
 
 	// string vazia
 	if(!str || !str[0]) return 1;
-
-	// pedido impossivel
-	if(is_prefix && have_port) return 2;
 
 	// tamanho da string
 	len=strlen(str);
 
 	// Formatos validos esperados:
-	// ::
-	// 1::
-	// ::1
-	// 2::2
-	// 2000::1234
-	// 1:2:3:4:5:6:7:8
-	// 2001:2002:2003:2004:2005:2006:2007:2008
-	// 2abc:2def:2abc:2ddd:2eee:2fff:2ded:dead
-	//
-	// ::/0
-	// 1::/7
-	// ::1/128
-	// 2::2/64
-	// 2000::1234/126
-	// 1:2:3:4:5:6:7:8/120
-	// 2001:2002:2003:2004:2005:2006:2007:2008/64
-	// 2abc:2def:2abc:2ddd:2eee:2fff:2ded:dead/126
+	// [::]:0 												6 bytes
+	// a
+	// [1111:2222:3333:4444:aaaa:bbbb:cccc:dddd]:65535 		47 bytes
+	if(len<6 || len > 47) return 2;
 
-	// [::]:1
-	// [::]:80
-	// [1::]:22
-	// [::1]:3209
-	// [2::2]:5900
-	// [2000::1234]:8080
-	// [1:2:3:4:5:6:7:8]:65535
-	// [2001:2002:2003:2004:2005:2006:2007:2008]:22
-	// [2abc:2def:2abc:2ddd:2eee:2fff:2ded:dead]:65535
+	// limpar a sala
+	memset( &(addr), 0, sizeof(addr) );
+	memset( &(comp), 0, sizeof(comp) );
+	for(i=0; i < len; i++){
+		char atch = str[i];
 
-	// []
-	// 0.0.0.0
-	// 0.0.0.0:65123
-	// 255.255.255.255:65535
-	// 0.0.0.0/0
-	// 255.255.255.255/32
+		// printf("ch=%c status=%d\n", atch, _status);
 
-	// GERAL
-	// menor formato: ::
-	// maior formato: [2abc:2def:2abc:2ddd:2eee:2fff:2ded:dead]:65535
-	if(len<2 || len > 47) return 3;
-
-	// COM PREFIXO
-	// menor formato: ::/0
-	// maior formato: 2abc:2def:2abc:2ddd:2eee:2fff:2ded:dead/126
-	if(is_prefix && ( len < 4 || len > 43 ) ) return 4;
-
-	// COM PORTA
-	// menor formato: [::]:1
-	// maior formato: [2abc:2def:2abc:2ddd:2eee:2fff:2ded:dead]:65535
-	if(have_port && ( len < 6 || len > 47 ) ) return 5;
-
-
-
-	// obter parte do endereco para facilitar leitura de prefixo e porta
-	//address = calloc(1, len+1);
-	//if(!address) exit(1);
-
-	// controles
-	int jumpcount = 0;
-	int comacount = 0;
-
-	// COLETAR ENDERECO IPV6
-	// loop
-	// printf("ENTRADA: '%s' [%d]\n", str, len);
-	for(i=0;i<len;i++){
-		// ler byte atual, proximo e anterior
-		atch = str[i];
-		lastch = nextch = n2xtch = 0;
-		if(i+1<len) nextch = str[i+1];
-		if(i+2<len) n2xtch = str[i+2];
-
-		// printf("> %c\n", atch);
-
-		// em modo porta, mas primeiro byte nao abrir endereco ipv6 []
-		if(have_port){
-			// precido que o primeiro byte abra o endereco
-			if(!i && atch != '[') return 6;
-			// abertura
-			if(!i && atch == '[') continue;
+		// abrindo
+		if(atch == '['){
+			if(_status!=0) return 3;
+			_status = 1; // abrindo
+			continue;
 		}
-
-		// se for byte valido, copiar
-		if( isxdigit(atch) || atch==':' ){
-			address[addrlen++] = atch;
+		// fechando
+		if(atch == ']'){
+			if(_status!=1) return 4;
+			_status = 2; // fechando
 			continue;
 		}
 
-		// detectar caracter proibido
-		if(atch!=']' && atch != ':' && atch != '/') return 7;
-
-		// esse byte nao faz mais parte do endereco
-		// marcar fim da parte de enderecos
-		if(atch==']' && nextch == ':'){
-			i+=2;
-		}else{
-			if(atch == '/' || atch == ']') i++;
-		}
-		rest = (str+i);
-		break;
-	}
-
-	// printf("Endereco: '%s' resto '%s'\n", address, rest);
-
-	// Loop byte a byte do endereco e valida-lo
-	for(i=0;i<addrlen;i++){
-		// ler byte atual, proximo e anterior
-		atch = address[i];
-		lastch = nextch = n2xtch = 0;
-		if(i+1<len) nextch = address[i+1];
-		if(i+2<len) n2xtch = address[i+2];
-
-		if(i) lastch = address[i-1];
-
-		// temos um JUMP?
-		if(atch==':' && nextch == ':'){
-			// tentou dar jump 2 vezes
-			if(jumpcount) return 8;
-			jumpcount++;
-
-			// especial:
-			// se o byte apos o jump for outro coma, temos uma violacao de formato
-			if(n2xtch ==':') return 9;
-
-			// ignorar o proximo
-			i++;
-			continue;
-
-		}
-
-		// separador
-		if(atch==':'){
-			comacount++;
-			// excedeu 8 grupos (7 separadores)
-			if(comacount>7 || (comacount > 5 && jumpcount) ) return 10;
-		}
-
-		// ler numero, so precisamos que ele seja coerente
-		if( isxdigit(atch) ){
-			int k = 0;
-			for(j=i; j<len; j++, k++){
-				char ch = address[j];
-				if(!isxdigit(ch)) break;
-				// posicionar em ultimo hexadecimal lido
-				i = j;
+		// encontramos a porta
+		if(atch == ':'){
+			if(_status==2){
+				commacount++;
+				if(commacount > 1) return 5;
+				// nao coletar esse byte
+				continue;
 			}
-			// tem mais de 4 digitos nesse numero
-			if(k>4) return 11;
-			continue;
 		}
 
-	} // for i
-
-	// grupos de menos
-	if(comacount != 7 && !jumpcount) return 12;
-
-
-	// ler numero apos endereco
-	if( (is_prefix || have_port)){
-		int num, rlen;
-		num = 0;
-
-		//printf("REST: %s\n", rest);
-
-		// sem bytes para processar
-		if(!rest || !rest[0]) return 13;
-		rlen = strlen(rest);
-
-		// transformar em numero
-		for(i=0;i<rlen;i++){
-			char ch;
-			ch = rest[i];
-
-			// nao e numero
-			if(!isdigit(ch)) return 14;
-			//printf(" RB> %c / %d\n", ch, num);
-
-			// numero
-			num *= 10;
-			num += (ch - 48);
+		// copiar byte para o endereco ou para
+		if(commacount){
+			// estamos lendo a porta
+			comp[idx2++] = atch;
+		}else{
+			// leitura de ipv6
+			// problema: nao estamos dentro do '[' .. ']'
+			if(_status!=1) return 7;
+			addr[idx1++] = atch;
 		}
-		port = num;
-		prefix = num;
-		//printf("Rest num: %d\n", num);
 	}
+	// printf("ADDR: '%s' COMP: '%s'\n", addr, comp);
 
-	//printf("POST=%d PREFIX=%d\n", port, prefix);
+	// parte de endereco e porta lidos, validar
+	// 1 - o ip 
+	if(!inet_pton(AF_INET6, addr, &(ipv6addr))) return 8;
 
-
-	// pediu prefix mas nao informou o prefixo
-	if(is_prefix && (prefix < 0 || prefix > 128)) return 15;
-
-	// pediu prefix mas nao informou o prefixo
-	if(have_port && (!port || port > 65535)) return 16;
+	// 2 - porta
+	if(!commacount || !idx2) return 9;
+	port = atoi(comp);
+	if(port < 1 || port > 65535) return 10;
 
 	return 0;
 }
-
-
 
 void help_std(){
 	printf("\n");
@@ -239,7 +91,7 @@ void help_std(){
 	printf("\n");
 	printf("Autor: Patrick Brandao <patrickbrandao@gmail.com>, todos os direitos reservados\n");
 	printf("\n");
-	printf("Use: is_ipv6 ([ipv6]:port) [[ipv6]:port] [[ipv6]:port] [...]\n");
+	printf("Use: is_ipv6port ([ipv6]:port) [[ipv6]:port] [[ipv6]:port] [...]\n");
 	printf("\n");
 	exit(1);
 }
@@ -248,7 +100,7 @@ int main(int argc, char **argv){
 	int i = 0;
     if(argc<2) help_std();
 	for(i=1;i<argc;i++){
-		int r = is_ipv6(argv[i], 0, 1);
+		int r = is_ipv6_port(argv[i]);
 		if(r>0) return r;
 	}
 	return 0;
