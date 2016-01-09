acab78ccea6bcd02b2b16ba9737c4d96bf169e25
Patrick Brandão <patrickbrandao@gmail.com>
2016-01-09 20:39:50 -0200

    Update is_ipv6prefix.c

--- is_ipv6prefix.c
+++ is_ipv6prefix.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 
+
 // testar se string esta no formato de rede ipv6
 int is_ipv6_prefix(char *str){
 	int i, len;
 	struct in6_addr ipv6addr;
-	char addr[INET6_ADDRSTRLEN];
-	char comp[INET6_ADDRSTRLEN];
+	char addr[INET6_ADDRSTRLEN + 4];
+	char comp[INET6_ADDRSTRLEN + 4];
 	int idx1 = 0, idx2 = 0;
 	int prefix = 0;
 	int barcount = 0;
@@ -20,9 +21,9 @@ int is_ipv6_prefix(char *str){
 	len=strlen(str);
 
 	// Formatos validos esperados:
-	// ::/0 										4 bytes
+	// ::/0 												4 bytes
 	// a
-	// 1111:2222:3333:4444:aaaa:bbbb:cccc:dddd/128 	43 bytes
+	// 1111:2222:3333:4444:aaaa:bbbb:cccc:dddd/128 			43 bytes
 	if(len<4 || len > 43) return 2;
 
 	// limpar a sala
@@ -30,30 +31,36 @@ int is_ipv6_prefix(char *str){
 	memset( &(comp), 0, sizeof(comp) );
 	for(i=0; i < len; i++){
 		char atch = str[i];
-		// separador de prefixo
+
+		// encontramos o prefixo
 		if(atch == '/'){
 			barcount++;
 			if(barcount > 1) return 3;
 			continue;
 		}
+
 		// copiar byte para o endereco ou para
 		if(barcount){
-			// leitura do prefixo
+			// estamos lendo a prefixo
 			comp[idx2++] = atch;
 		}else{
-			// leitura de ip
+			// leitura de ipv6
 			addr[idx1++] = atch;
 		}
 	}
+	// printf("ADDR: '%s' COMP: '%s'\n", addr, comp);
 
-	// parte de endereco e porta lidos, validar
+	// parte de endereco e prefixo lidos, validar
 	// 1 - o ip 
-	if(!inet_pton(AF_INET6, addr, &(ipv6addr))) return 4;
+	if(!inet_pton(AF_INET6, addr, &(ipv6addr))) return 5;
 
 	// 2 - prefixo
-	if(!barcount || !idx2) return 5;
+	if(!barcount || !idx2) return 6;
 	prefix = atoi(comp);
-	if(prefix < 0 || prefix > 128) return 6;
+
+	// printf("Prefixo: %d\n", prefix);
+
+	if(prefix < 0 || prefix > 128) return 7;
 
 	return 0;
 }
@@ -76,7 +83,7 @@ int main(int argc, char **argv){
     if(argc<2) help_std();
 	for(i=1;i<argc;i++){
 		int r = is_ipv6_prefix(argv[i]);
-		if(r>0) return r;
+		if(r > 0) return r;
 	}
 	return 0;
 }
